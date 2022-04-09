36546d109bc2ca2411be258f4cf7cea287ff8689
datphamcode295 <vandatpro2000@gmail.com>
2022-04-09 17:42:40 +0700

    user2@gmail.com 123456789

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,58 +1,107 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView,SafeAreaView } from 'react-native';
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput, Button,TouchableOpacity, Text } from "react-native";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from "../firebase-config";
+
+//https://www.youtube.com/watch?v=9pq9yR9nEqo
+
+export default function LoginScreen({navigation})  {
+  const [email, onChangeEmail] = React.useState("");
+  const [pass, onChangePass] = React.useState("");
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
 
 
 
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, pass)
+    .then((userCredential) => {
+      console.log('Signed in!')
+      const user = userCredential.user;
+      navigation.navigate('Home')
 
- 
+    //   console.log(user)
+    
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+  const handleCreateAccount = () => {
+    createUserWithEmailAndPassword(auth, email, pass)
+    .then((userCredential) => {
+      console.log('Account created!')
+      const user = userCredential.user;
+      console.log(user)
+    })
+    .catch(error => {
+      console.log(error)
+      Alert.alert(error.message)
+    })
+  }
 
+  const func = () => {
+    console.log(email + ' ' + pass)
+    onChangeEmail("")
+    onChangePass("")
+    handleSignIn()
+  }
 
-export default function LoginScreen() {
   return (
-<SafeAreaView style={styles.container}> 
-   <Text>Login</Text>
-      
-      
-</SafeAreaView>
-      
-   
+    <SafeAreaView style={styles.container}>
+    
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeEmail}
+        value={email}
+        placeholder="Email"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangePass}
+        value={pass}
+        placeholder="Password"
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={func}
+      >
+          <Text>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={handleCreateAccount}
+        style={styles.button}
+      >
+          <Text>Create Account</Text>
+      </TouchableOpacity>
+
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding:5,
-  },
-  scrollView: {
-    width:'100%',
-    backgroundColor: 'pink',
-  },
-  blockcontainer: {
-    flex: 6, 
-    justifyContent: 'center', 
-    alignItems: 'center',
-    width:370, 
-    backgroundColor: '#88B8DA',
-  },
-  dashboardcontainer: {
-    flex: 3, 
-    flexDirection:'row',
-    justifyContent: 'space-between', 
-    alignItems: 'center',
-    width:370, 
-    // backgroundColor: '#88B8DA',
-  },
-  titleText: {
-    fontSize: 40,
-    fontWeight: "bold"
-  },
-  modalContainer:{
-    flex:1,
-    flexDirection: "row",
-    flexWrap: 'wrap',
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding:5,
+      },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        width: 250,
+        padding: 10,
+    },
+    button:{
+        alignItems: "center",
+        backgroundColor: "#76b5c5",
+        padding: 10,
+        width: 150,
+        marginTop:5
+    }
 });
+
