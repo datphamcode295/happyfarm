e39bc3d3b412f2ea5b900e56009d789ba9ddc2b5
vonhatthanh2000 <nthanhatee@gmail.com>
2022-04-10 23:16:48 +0700

    adding display

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView,SafeAreaView } from 'react-native';
-
-import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+
+import Dashboard from './screens/Dashboard';
+import Settings from './screens/Settings';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -16,6 +18,9 @@ export default function App() {
     <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen name="Login" component={LoginScreen}/>
+      <Stack.Screen name="Home Page" component={Dashboard}/>
+      <Stack.Screen name="Settings" component={Settings}/>
+      
       <Stack.Screen name="Home" component={HomeScreen} />
     </Stack.Navigator>
   </NavigationContainer>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, TextInput, Button,TouchableOpacity, Text } from "react-native";
+import { SafeAreaView, StyleSheet, TextInput, Button,TouchableOpacity, Text, View,Dimensions } from "react-native";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from "../firebase-config";
+import {StatusBar} from 'expo-status-bar';
+import Svg, { Path, Defs, LinearGradient, Stop } from "react-native-svg";
+import ButtonGradient from './ButtonGradient';
+const {width, height} =Dimensions.get('window')
+
+
 
 //https://www.youtube.com/watch?v=9pq9yR9nEqo
 
@@ -20,7 +26,7 @@ export default function LoginScreen({navigation})  {
     .then((userCredential) => {
       console.log('Signed in!')
       const user = userCredential.user;
-      navigation.navigate('Home')
+      navigation.navigate('Home Page')
 
     //   console.log(user)
     
@@ -49,46 +55,110 @@ export default function LoginScreen({navigation})  {
     handleSignIn()
   }
 
-  return (
-    <SafeAreaView style={styles.container}>
-    
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeEmail}
-        value={email}
-        placeholder="Email"
+  function SvgTop() {
+    return (
+      <Svg
+      width={500}
+      height={324}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <Path
+        d="M297.871 315.826c73.405 13.896 165.338-13.964 202.129-29.63V230H1.326v63.5c69.15-42.913 204.789 4.957 296.545 22.326z"
+        fill="url(#prefix__paint0_linear_103:6)"
+        fillOpacity={0.5}
       />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangePass}
-        value={pass}
-        placeholder="Password"
+      <Path
+        d="M237.716 308.627C110.226 338.066 30.987 318.618 0 304.77V0h500v304.77c-43.161-12.266-134.794-25.581-262.284 3.857z"
+        fill="url(#prefix__paint1_linear_103:6)"
       />
-      <TouchableOpacity
-        style={styles.button}
-        onPress={func}
-      >
-          <Text>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={handleCreateAccount}
-        style={styles.button}
-      >
-          <Text>Create Account</Text>
-      </TouchableOpacity>
+      <Defs>
+        <LinearGradient
+          id="prefix__paint0_linear_103:6"
+          x1={492.715}
+          y1={231.205}
+          x2={480.057}
+          y2={364.215}
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stopColor="#2EB62C" />
+          <Stop offset={1} stopColor="#83D475" />
+        </LinearGradient>
+        <LinearGradient
+          id="prefix__paint1_linear_103:6"
+          x1={15.304}
+          y1={4.155}
+          x2={144.016}
+          y2={422.041}
+          gradientUnits="userSpaceOnUse"
+        >
+          <Stop stopColor="#2EB62C" />
+          <Stop offset={1} stopColor="#83D475" />
+        </LinearGradient>
+      </Defs>
+    </Svg>
+    )
+  }
 
+  return (
+    
+    <SafeAreaView style={styles.mainContainer}>
+      <View style={styles.containerSVG}>
+        <SvgTop/>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.titulo}>HappyFarm</Text>
+        <TextInput 
+          onChangeText={onChangeEmail}
+          value={email}
+          placeholder="Email"
+          style={styles.textInput}
+        />
+
+        <TextInput 
+          onChangeText={onChangePass}
+          value={pass}
+          placeholder="Password"
+          style={styles.textInput}
+        />
+      <Text style={styles.forgotPassword}>Forgot your password?</Text>
+      <TouchableOpacity
+    style={styles.button}
+       onPress={func}
+      >
+         <Text fontWeight='bold'>Login</Text>
+         </TouchableOpacity>
+        {/* <Text style={styles.forgotPassword}>Don't have an account?</Text> */}
+        <TouchableOpacity
+         onPress={handleCreateAccount}
+         style={styles.button}
+       >
+           <Text>Create Account</Text>
+       </TouchableOpacity>
+        <StatusBar style="auto" />
+      </View>
     </SafeAreaView>
+    
   );
 };
 
 const styles = StyleSheet.create({
+    mainContainer: {
+      backgroundColor: '#f1f1f1',
+      flex: 1,
+      padding:5,
+    },
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding:5,
-      },
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    containerSVG: {
+      width: width,
+      justifyContent: 'center',
+      alignItems: 'center',
+      flex:0.8,
+    },
+  
     input: {
         height: 40,
         margin: 12,
@@ -98,10 +168,30 @@ const styles = StyleSheet.create({
     },
     button:{
         alignItems: "center",
-        backgroundColor: "#76b5c5",
+        backgroundColor: "#83D475",
         padding: 10,
         width: 150,
-        marginTop:5
-    }
+        marginTop:5,
+    },
+    titulo:{
+      fontSize: 40,
+      color: '#34434D',
+      fontWeight: 'bold',
+    },
+    textInput: {
+      borderWidth:1,
+      padding: 10,
+      paddingStart: 30,
+      width: '80%',
+      height: 50,
+      marginTop: 20,
+      borderRadius: 30,
+      backgroundColor: '#fff',
+    },
+    forgotPassword: {
+      fontSize: 14,
+      color: 'gray',
+      marginTop: 20
+    },
 });
 
